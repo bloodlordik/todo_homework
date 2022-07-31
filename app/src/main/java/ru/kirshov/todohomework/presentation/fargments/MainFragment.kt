eab94d51fefdd4796a7e345ba28d0cee59b0c3b6
Roma Kirshiv <bloodlordik@yandex.ru>
2022-07-31 20:55:42 +0700

    Add Floating button

--- app/src/main/java/ru/kirshov/todohomework/presentation/fargments/MainFragment.kt
+++ app/src/main/java/ru/kirshov/todohomework/presentation/fargments/MainFragment.kt
@@ -1,7 +1,20 @@
 package ru.kirshov.todohomework.presentation.fargments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import org.koin.androidx.navigation.koinNavGraphViewModel
 import ru.kirshov.todohomework.R
+import ru.kirshov.todohomework.databinding.FargmentMainBinding
+import ru.kirshov.todohomework.domain.MainViewModel
 
 class MainFragment:Fragment(R.layout.fargment_main) {
+    private val navController:NavController by lazy { findNavController() }
+    private val mainViewModel by koinNavGraphViewModel<MainViewModel>(R.id.navigation_graf)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
