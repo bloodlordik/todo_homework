053e8a80bd18e710186af7700a88d12b19b07c2a
Roma Kirshiv <bloodlordik@yandex.ru>
2022-08-13 09:26:50 +0700

    Add color pallet

--- app/src/main/java/ru/kirshov/todohomework/presentation/fargments/DetailFragment.kt
+++ app/src/main/java/ru/kirshov/todohomework/presentation/fargments/DetailFragment.kt
@@ -3,5 +3,5 @@ package ru.kirshov.todohomework.presentation.fargments
 import androidx.fragment.app.Fragment
 import ru.kirshov.todohomework.R
 
-class DetailFragment:Fragment(R.layout.fargment_detail) {
+class DetailFragment:Fragment(R.layout.fragment_detail) {
 }
--- app/src/main/java/ru/kirshov/todohomework/presentation/fargments/MainFragment.kt
+++ app/src/main/java/ru/kirshov/todohomework/presentation/fargments/MainFragment.kt
@@ -2,26 +2,32 @@ package ru.kirshov.todohomework.presentation.fargments
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.navigation.koinNavGraphViewModel
 import ru.kirshov.todohomework.R
-import ru.kirshov.todohomework.databinding.FargmentMainBinding
+import ru.kirshov.todohomework.databinding.FragmentMainBinding
 import ru.kirshov.todohomework.domain.MainViewModel
 
-class MainFragment:Fragment(R.layout.fargment_main) {
-    private val binding:FargmentMainBinding by BindingDelegate(FargmentMainBinding::bind)
+class MainFragment:Fragment(R.layout.fragment_main) {
+    private var _binding: FragmentMainBinding? = null
+    private val binding:FragmentMainBinding get() = _binding!!
     private val navController:NavController by lazy { findNavController() }
     private val mainViewModel by koinNavGraphViewModel<MainViewModel>(R.id.navigation_graf)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
          binding.fab.setOnClickListener {
 
             navController.navigate(R.id.action_mainFragment_to_detailFragment)
         }
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
